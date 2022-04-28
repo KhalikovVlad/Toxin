890c75f8fe280e23061472b4b6560309dbd104e5
khalikovvlad <khalikovvlad@gmail.com>
2022-04-28 11:37:06 +0700

    added new improved script

--- src/components/pie-chart/pie-chart.js
+++ src/components/pie-chart/pie-chart.js
@@ -1,22 +1,22 @@
-import Chart from 'chart.js';
+import Chart from "chart.js";
+
+const pieChart = document.querySelector('.js-pie-chart');
+const labels = JSON.parse(pieChart.dataset.labels);
+const colors = JSON.parse(pieChart.dataset.colors);
+const counts = JSON.parse(pieChart.dataset.counts).map((item) => Number(item))
 
 const ctx = document.getElementById('myChart').getContext('2d');
 
 new Chart(ctx, {
     type: 'pie',
     data: {
-        labels: ['Разочарован', 'Удовлетворительно', 'Хорошо', 'Великолепно'],
+        labels: labels,
         datasets: [{
             label: 'Votes',
-            backgroundColor: [
-                'rgba(144, 144, 144)', 
-                'rgba(188, 156, 255)', 
-                'rgba(111, 207, 151)', 
-                'rgba(255, 227, 156)'
-            ],
+            backgroundColor: colors,
             borderColor: 'white',
             borderWidth: 2,
-            data: [0, 65, 65, 130]
+            data: counts
         }]
     },
     options: {
