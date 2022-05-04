c2ee408582ae87b56a4de4ae82b4cff176095388
khalikovvlad <khalikovvlad@gmail.com>
2022-05-04 12:15:12 +0700

    added dateMask

--- src/components/text-field/text-field.js
+++ src/components/text-field/text-field.js
@@ -1,15 +1,50 @@
-import IMask, { Masked } from 'imask';
+import Cleave from 'cleave.js';
 
-var lazyMask = IMask(
-    document.getElementById('mask'),
-    {
-        mask: Date,
-        lazy: false,
-        autofix: true,
-        blocks: {
-            d: {mask: IMask.MaskedRange, placeholderChar: 'Д', from: 1, to: 31, maxLength: 2},
-            m: {mask: IMask.MaskedRange, placeholderChar: 'М', from: 1, to: 12, maxLength: 2},
-            Y: {mask: IMask.MaskedRange, placeholderChar: 'Г', from: 1900, to: 2023, maxLength: 4}
-        }
+const textField = document.querySelector('.js-text-field');
+const input = textField.querySelector('.js-text-field__input');
+const hiddenInput = textField.querySelector('.js-text-field__hidden-input');
+const inputPlaceholder = input.dataset.placeholder;
+const dateMask = input.hasAttribute('data-date-mask');
+const inputTitle = input.dataset.title;
+
+
+function relocateHiddenInputFocus() {
+    hiddenInput.addEventListener('focus', (e) => input.focus());
+};
+
+function setDateMask() {
+    new Cleave(input, {
+        date: true,
+        dateMin: '1900-01-01',
+        dateMax: '2022-12-31',
+        datePattern: ["d", "m", "Y"],
+        delimiter: '.'
+    });
+};
+
+if (dateMask) {
+    setDateMask();
+};
+if (hiddenInput) {
+    relocateHiddenInputFocus();
+};
+ 
+function setPlaceholder(placeholder) {
+    if (input.type === 'button') {
+        input.value = placeholder ? placeholder : inputPlaceholder;
+    } else {
+        placeholder ? input.setAttribute('placeholder', placeholder) : input.setAttribute('placeholder', inputPlaceholder);
     }
-);
+};
+
+function setTitle(title) {
+    title ? input.setAttribute('title', title) : input.setAttribute('title', inputTitle);
+};
+
+function setValue(value) {
+    if (input.getAttribute('type') === 'button') {
+        hiddenInput.value = value ? value : '';
+    } else {
+        input.value = value ? value : '';
+    }
+};
