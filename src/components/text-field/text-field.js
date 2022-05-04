da63f3d46ef9d26b5291a2b98fb5dc33e1ad2142
khalikovvlad <khalikovvlad@gmail.com>
2022-05-04 12:17:40 +0700

    removed unnecessary things

--- src/components/text-field/text-field.js
+++ src/components/text-field/text-field.js
@@ -3,9 +3,7 @@ import Cleave from 'cleave.js';
 const textField = document.querySelector('.js-text-field');
 const input = textField.querySelector('.js-text-field__input');
 const hiddenInput = textField.querySelector('.js-text-field__hidden-input');
-const inputPlaceholder = input.dataset.placeholder;
 const dateMask = input.hasAttribute('data-date-mask');
-const inputTitle = input.dataset.title;
 
 
 function relocateHiddenInputFocus() {
@@ -27,24 +25,4 @@ if (dateMask) {
 };
 if (hiddenInput) {
     relocateHiddenInputFocus();
-};
- 
-function setPlaceholder(placeholder) {
-    if (input.type === 'button') {
-        input.value = placeholder ? placeholder : inputPlaceholder;
-    } else {
-        placeholder ? input.setAttribute('placeholder', placeholder) : input.setAttribute('placeholder', inputPlaceholder);
-    }
-};
-
-function setTitle(title) {
-    title ? input.setAttribute('title', title) : input.setAttribute('title', inputTitle);
-};
-
-function setValue(value) {
-    if (input.getAttribute('type') === 'button') {
-        hiddenInput.value = value ? value : '';
-    } else {
-        input.value = value ? value : '';
-    }
 };
