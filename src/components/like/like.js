18e6738e2dc6bd4ea822d9dd9ff2bd2e4ebe383e
khalikovvlad <khalikovvlad@gmail.com>
2022-05-14 14:06:18 +0700

    added RU lang, js updated (bug fix)

--- src/components/like/like.js
+++ src/components/like/like.js
@@ -1,13 +1,18 @@
-const likeBtn = document.querySelector('.js-like');
-const likesCount = document.querySelector('.js-like__count');
+document.querySelectorAll('.js-like').forEach(function (likeWrapper) {
+    const likeCount = likeWrapper.querySelector('.js-like__count');
+    const likeIcon = likeWrapper.querySelector('.js-like__icon');
 
+    likeWrapper.onclick = function  () {
+        if (likeWrapper.classList.contains('like_active')) {
+            likeCount.textContent--;
+            likeIcon.classList.remove('_icon-favorite');
+            likeIcon.classList.add('_icon-favorite_border');         
+        } else {
+            likeCount.textContent++;
+            likeIcon.classList.remove('_icon-favorite_border');
+            likeIcon.classList.add('_icon-favorite');
+        };
 
-likeBtn.onclick = function  () {
-    if (likeBtn.classList.contains('like_active')) {
-        likesCount.textContent--;
-    } else {
-        likesCount.textContent++;
-    }
-    likeBtn.classList.toggle('like_active');
-
-};
+        likeWrapper.classList.toggle('like_active');     
+    };
+});
