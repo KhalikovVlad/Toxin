1e395c3d06d41917da44d4e00723852b2e6bacf8
khalikovvlad <khalikovvlad@gmail.com>
2022-05-14 14:17:10 +0700

    name changed, spaces removed, RU lang added

--- src/components/range-slider/range-slider.js
+++ src/components/range-slider/range-slider.js
@@ -4,23 +4,21 @@ import 'nouislider/distribute/nouislider.min.css';
 const currencyFormat = require('../../globals/helpers/currencyFormat.js');
 
 const rangeSlider = document.querySelector('.js-slider');
-const sliderRange = rangeSlider.querySelector('.js-slider__range');
-const input = rangeSlider.querySelector('.js-slider__input');
-const lang = sliderRange.dataset.lang;
-const i18n = require('../../components/range-slider/i18n.json')[lang];
+const rangeSliderRange = rangeSlider.querySelector('.js-slider__range');
+const rangeSliderInput = rangeSlider.querySelector('.js-slider__input');
 const rangeData = {
     range: {
-        'min': Number(JSON.parse(sliderRange.dataset.range)[0]),
-        'max': Number(JSON.parse(sliderRange.dataset.range)[1])
+        'min': Number(JSON.parse(rangeSliderRange.dataset.range)[0]),
+        'max': Number(JSON.parse(rangeSliderRange.dataset.range)[1])
     },
-    initValue: JSON.parse(sliderRange.dataset.initValue),
-    step: Number(sliderRange.dataset.step),
-    margin: Number(sliderRange.dataset.margin)
+    initValue: JSON.parse(rangeSliderRange.dataset.initValue),
+    step: Number(rangeSliderRange.dataset.step),
+    margin: Number(rangeSliderRange.dataset.margin)
 };
 
 const updateInputValue = (values) => {
     const inputValue = values.reduce((result, value) => `${result}${value} - `, '').slice(0, -2);
-    input.value = inputValue;
+    rangeSliderInput.value = inputValue;
 };
 function setCustomClasses() {
     noUiSlider.cssClasses.base += ' slider__base';
@@ -29,16 +27,13 @@ function setCustomClasses() {
     noUiSlider.cssClasses.tooltip += ' slider__tooltip';
 };
 function setA11lyToHandles() {
-    const { RANGE_FROM, RANGE_TO } = i18n;
     const handles = document.querySelectorAll('.js-slider__handle');
 
-    handles[0].setAttribute('aria-label', RANGE_FROM);
-    handles[1].setAttribute('aria-label', RANGE_TO);
+    handles[0].setAttribute('aria-label', 'Диапазон от');
+    handles[1].setAttribute('aria-label', 'Диапазон до');
 }
 function setSlider() {
-    const { CURRENCY } = i18n;
-
-    noUiSlider.create(sliderRange, {
+    noUiSlider.create(rangeSliderRange, {
         connect: true,
         start: rangeData.initValue,
         step: rangeData.step,
@@ -46,11 +41,11 @@ function setSlider() {
         range: rangeData.range,
         tooltips: true,
         format: {
-            to: (value) => currencyFormat(lang, CURRENCY, value),
+            to: (value) => currencyFormat('RUB', value),
             from: (value) => Number(value.replace(' - ', ''))
         }
     });
-    sliderRange.noUiSlider.on('update', updateInputValue);
+    rangeSliderRange.noUiSlider.on('update', updateInputValue);
 };
 setCustomClasses();
 setSlider();
